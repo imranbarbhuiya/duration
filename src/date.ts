12391d3ef4872894c86fc3c9db64b5b93cb17637
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-03-16 08:25:27 +0530

    fix: padLeftZero properly

--- src/date.ts
+++ src/date.ts
@@ -1,7 +1,7 @@
 import { dateFormateRegex, type dateFormats, months } from './constants';
 
-const padLeftZero = (str: number) => {
-  return `0${str}`.slice(-2);
+const padLeftZero = (num: number) => {
+  return Math.abs(num) < 10 ? `0${num}` : `${num}`;
 };
 
 /**
