20f6c38f6c1f77b37cc29b67d9d75d0bede5fa88
parbez <imranbarbhuiya.fsd@gmail.com>
2022-05-04 05:50:59 +0530

    chore: update jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  displayName: 'unit test',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRunner: 'jest-circus/runner',
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tests/tsconfig.json',
-    },
-  },
+	displayName: 'unit test',
+	preset: 'ts-jest',
+	testMatch: ['<rootDir>/tests/**/*.test.ts'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tests/tsconfig.json'
+		}
+	},
+	reporters: ['default', 'github-actions']
 };
 
 export default config;
